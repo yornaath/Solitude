2af9d33eb982712fb7d29dc7e0941216ad461de7
andtan <jornandretangen@gmail.com>
2011-02-25 20:58:53 +0100

    stylechanges: icons on pin and remove tweet, new icons and a logo in banner

--- appdirectory-build/lib/javascript/app.build.js
+++
@@ -1,1 +0,0 @@
-{appDir:"../../",baseUrl:"lib/javascript",dir:"../../appdirectory-build",modules:[{name:"main",name:"jquery.require",name:"twitterFeed/twitterFeed",name:"twitterFeedController",name:"doc/docModel",name:"doc/docController",name:"util/gears"}]}
